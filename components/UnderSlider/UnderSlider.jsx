25f8d1a3b5b03f1941e8d403cf15c0c8295fdb17
Izatis <mekinovizat1@gmail.com>
2023-03-31 04:02:35 +0600

    add: get request

--- components/UnderSlider/UnderSlider.jsx
+++ components/UnderSlider/UnderSlider.jsx
@@ -1,5 +1,6 @@
-﻿import React from "react";
+﻿import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import axios from 'axios'
 
 import "swiper/css";
 import "swiper/css/pagination";
@@ -10,46 +11,39 @@ import "swiper/css/pagination";
 import { FreeMode, Navigation } from "swiper";
 
 export default function UnderSLider() {
-    const slides = [
-        {
-            img: '/slide1.jpg'
-        },
-        {
-            img: '/slide2.jpg'
-        },
-        {
-            img: '/slide3.jpg'
-        },
-        {
-            img: '/slide4.jpg'
-        },
-        {
-            img: '/slide5.jpg'
-        },
-    ]
-    return (
-        <>
-            <Swiper
-                loop={true}
-                centeredSlides={true}
-                spaceBetween={30}
-                freeMode={true}
-                slidesPerView={1}
-                breakpoints={{
-                    600: {
-                        slidesPerView: "1.5"
-                    }
-                }}
-                navigation={true}
-                modules={[FreeMode, Navigation]}
-                className="Under_slider"
-            >
-                {slides.map((slide) => (
-                    <SwiperSlide>
-                        <img src={slide.img} alt="" />
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-        </>
-    );
+  const [dataSlide, setDataSlide] = useState([]);
+
+  useEffect(() => {
+    try {
+      axios.get("http://159.65.144.176/api/events/").then((response) => setDataSlide(response.data));
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+ 
+  return (
+    <>
+      <Swiper
+        loop={true}
+        centeredSlides={true}
+        spaceBetween={30}
+        freeMode={true}
+        slidesPerView={1}
+        breakpoints={{
+          600: {
+            slidesPerView: "1.5",
+          },
+        }}
+        navigation={true}
+        modules={[FreeMode, Navigation]}
+        className="Under_slider"
+      >
+        {dataSlide.map((slide) => (
+          <SwiperSlide>
+            <img src={slide.image} alt="" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
 }
