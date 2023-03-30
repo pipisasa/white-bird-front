53555aa51930dde117bc90602704548e625223e0
Mhan <whitehanclean@gmail.com>
2023-03-31 03:55:47 +0600

    fix kera blocks

--- components/BirdVideo/BirdVideo.jsx
+++ components/BirdVideo/BirdVideo.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import s from "./BirdVideo.module.scss";
 
-
 const BirdVideo = () => {
   return (
     <div className={s.birdVideo}>
-      <video controls muted style={{ width: "100%", margin: "0 auto", borderRadius: '20px' }}>
-        <source src="/miyajima-33821-1.mp4" type="video/mp4"/>
+      <h2>Видео о нас</h2>
+      <video
+        controls
+        muted
+        style={{ width: "100%", margin: "0 auto", borderRadius: "20px" }}
+      >
+        <source src="/miyajima-33821-1.mp4" type="video/mp4" />
       </video>{" "}
     </div>
   );
